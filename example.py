66739620941b13714f30c01fa9adb1348b10b756
Nev <65242433+0xNev@users.noreply.github.com>
2022-11-14 17:43:38 -0500

    rename

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
-import harvester
+import pyharvester
 
 #Create a new harvester instace 
-harvester_1 = harvester.new(solver_type="recaptchav3", url="https://recaptcha-demo.appspot.com/recaptcha-v3-request-scores.php", site_key="6LdyC2cUAAAAACGuDKpXeDorzUDWXmdqeg-xy696", action="examples/v3scores")
+harvester_1 = pyharvester.new(solver_type="recaptchav3", url="https://recaptcha-demo.appspot.com/recaptcha-v3-request-scores.php", site_key="6LdyC2cUAAAAACGuDKpXeDorzUDWXmdqeg-xy696", action="examples/v3scores")
 
 #Start a new solve
 captcha_token = harvester_1.solve(timeout=100)
